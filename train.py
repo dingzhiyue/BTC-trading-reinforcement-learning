e4c880bb6ddfed6f826c7dc2e58889007cfce16d
dingzhiyue <dingzhiyue@gmail.com>
2020-06-25 13:01:33 -0500

    Add files via upload

---
+++ train.py
@@ -0,0 +1,172 @@
+import gym
+import numpy as np
+import random
+import pandas as pd
+import numpy as np
+from envs.environment_gym import trading_environment
+import matplotlib.pyplot as plt
+import keras as ks
+from keras.layers import Dense
+import collections
+
+class DQN:
+    def __init__(self, env):
+        self.env = env
+        self.pool_size=50
+        self.learning_rate = 0.01
+        self.epsilon = 0.1
+        self.batch_size = 32
+        self.alpha = 0.5
+        self.gamma = 0.01
+        self.experience_pool = collections.deque(maxlen=self.pool_size)
+        self.model = self.build_model()
+        self.target_model = self.model
+
+    def build_model(self):
+        print(self.pool_size)
+        model = ks.models.Sequential()
+        input_size = self.env.observation_space.shape[0] * self.env.windows
+        #input_size = 16
+        output_size = 21
+
+        model.add(Dense(100, input_dim=input_size, activation='relu'))
+        model.add(Dense(25, activation='relu'))
+        model.add(Dense(output_size))
+
+        model.compile(loss='mean_squared_error', optimizer=ks.optimizers.Adam(lr=self.learning_rate))
+        return model
+
+    def epsilon_greedy_action(self,state):
+        rd = np.random.uniform()
+        if rd >= self.epsilon:
+            state = self.transfer_state_dim(state)
+            act = np.argmax(self.model.predict(state))
+        else:
+            act = self.env.action_space.sample()
+        return act
+
+    def transfer_state_dim(self, state):
+        out = np.reshape(state, [-1, state.shape[0] * state.shape[1]])
+        return out
+
+
+    def save_into_pool(self, state, act):
+        state_next, rewards, done, info = self.env.step(act)
+        new_sample = [state, act, rewards, state_next, done]
+        self.experience_pool.append(new_sample)
+        return state_next, rewards, done
+
+    def initial_pool(self):
+        i = 0
+        while True:
+            if i > self.pool_size:
+                break
+            state = self.env.reset()
+            while True:
+                i += 1
+                act = self.env.action_space.sample()
+                state_next, rewards, done, info = self.env.step(act)
+                self.save_into_pool(state, act)
+                state = state_next
+                if done or i > self.pool_size:
+                    break
+
+    def sample_from_pool(self):
+        training_samples = random.sample(self.experience_pool, self.batch_size)
+        return training_samples
+
+    def train_batch(self, training_samples):
+        #train_x = []
+        #train_y = []
+        for sample in training_samples:
+            state, act, reward, state_next, done = sample
+            state = self.transfer_state_dim(state)
+            #train_x.append(state)
+            state_next = self.transfer_state_dim(state_next)
+            target = self.target_model.predict(state)
+            if done:
+                target[0][act] = reward
+            else:
+                target[0][act] = reward + self.gamma * np.max(self.target_model.predict(state_next))
+            #train_y.append(target)
+
+            self.model.fit(state, target, epochs=1)
+
+    def update_target_model(self):
+        weights = self.model.get_weights()
+        target_weights = self.target_model.get_weights()
+        #self.target_model.set_weights(weights)#simply update by weights
+        for i in range(len(weights)):
+            target_weights[i] = (1 - self.alpha) * target_weights[i] + self.alpha * weights[i]
+        self.target_model.set_weights(target_weights)
+
+    def model_save(self, name):
+        self.model.save(name)
+
+
+def DQN_train():
+    market_data_path = 'data/coinbase_daily.csv'
+    df = pd.read_csv(market_data_path)
+
+    df_filter = df[['Open','High','Low','Close','Volume BTC']]
+    market_data = df_filter.values[::-1]
+
+
+    initial_cash_balance = 10000
+    initial_bt_balance = 0
+    windows = 10
+    env = trading_environment(market_data, windows=windows, initial_cash_balance=initial_cash_balance, initial_bt_balance=initial_bt_balance)
+    DQN_agent = DQN(env)
+
+    trails = 10
+    iterations = 100
+
+    DQN_agent.initial_pool()
+    print('initial pool finished')
+    for trail in range(trails):
+        state = env.reset()
+        for iteration in range(iterations):
+            act = DQN_agent.epsilon_greedy_action(state)#下一步
+            state_next, rewards, done = DQN_agent.save_into_pool(state, act)#save into pool同时做一步step
+            training_samples = DQN_agent.sample_from_pool()#sample from pool
+            DQN_agent.train_batch(training_samples)#train batch
+            if iteration%10==0:#update target NN every10 steps
+                DQN_agent.update_target_model()
+            state = state_next
+            if done:
+                break
+    DQN_agent.model_save('BTC_DQN')
+
+if __name__=='__main__':
+    DQN_train()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
